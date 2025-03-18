8a51beeb692a14f477fd977827258dbc9563c48a
Arsen <113983703+Arsenk0@users.noreply.github.com>
2025-03-18 18:37:54 +0200

    Delete Lab1-3 directory

--- Lab1-3/Lab1/creational/prototype.py
+++
@@ -1,8 +0,0 @@
-import copy
-
-class Prototype:
-    def __init__(self, name):
-        self.name = name
-
-    def clone(self):
-        return copy.deepcopy(self)
--- Lab1-3/Lab2/structural/flyweight.py
+++
@@ -1,9 +0,0 @@
-# flyweight.py
-
-class Flyweight:
-    def operation(self, extrinsic_state):
-        pass
-
-class ConcreteFlyweight(Flyweight):
-    def operation(self, extrinsic_state):
-        return f"ConcreteFlyweight: operation with state {extrinsic_state}."
